a778bbbc9fe91861359934eeff2b6c96c9609e29
Gabriel Araujo <aaraujoslgabriel@gmail.com>
2025-07-25 18:00:39 -0300

    📦 Limpeza do código para deixar com uma estrtutura inicial limpa para o desenvolvimento da API.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+// Criando o construtor da aplicação.
+var builder = WebApplication.CreateBuilder(args);
+
+// -- Configura serviços com o constutor da aplicação (injeção de dependência). -- 
+
+// Faz a API "saber" que possui endpoints.
+builder.Services.AddControllers();
+
+// Habilita a descoberta automática de endpoints para o Swagger
+builder.Services.AddEndpointsApiExplorer();
+
+// Adiciona o gerador de documentação Swagger/OpenAPI.
+builder.Services.AddSwaggerGen();
+
+// -------------------------------------------------------------------------------
+
+// Pega todas as configurações feitas (serviços, middlewares) e cria a aplicação final.
+var app = builder.Build();
+
+// Verifica se a aplicação está num ambiente de desenvolvimento.
+if (app.Environment.IsDevelopment())
+{
+    // Habilita o Swagger.
+    app.UseSwagger();
+
+    //Adiciona a interface gráfica do Swagger para testes.
+    app.UseSwaggerUI();
+}
+
+// Força a API a redirecionar de HTTP para HTTPS.
+app.UseHttpsRedirection();
+
+// Ativa a verificação de permissões (ex.: endpoints que só usuários logados podem acessar).
+// OBS: Para funcionar, você precisa configurar autenticação também.
+app.UseAuthorization();
+
+// Faz o link entre as rotas HTTP e os controllers que você criou.
+app.MapControllers();
+
+// Inicia o servidor web (Kestrel) e fica esperando requisições.
+app.Run();
